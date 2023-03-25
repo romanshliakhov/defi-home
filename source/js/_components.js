39f9fd19ed24caf5526d2055307225b76e77b1f2
Roman <romanshliakhov@gmail.com>
2023-03-25 18:14:05 +0200

    update main page

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,10 +1,10 @@
 import './components/sliders.js';
-import './components/select.js';
-import './components/burger.js';
+// import './components/select.js';
+// import './components/burger.js';
 import './components/spollers.js';
-import './components/dropdown.js';
-import './components/tabs.js';
-import './components/modals.js';
+// import './components/dropdown.js';
+// import './components/tabs.js';
+// import './components/modals.js';
 
 
 
