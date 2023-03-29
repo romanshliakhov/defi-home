b5caee86e38b22fa8735aef146bf30b868da607e
Roman <romanshliakhov@gmail.com>
2023-03-29 15:12:16 +0300

    add ratings

--- source/js/_components.js
+++ source/js/_components.js
@@ -3,6 +3,7 @@ import './components/select.js';
 // import './components/burger.js';
 import './components/spollers.js';
 import './components/fullFeedback.js';
+import './components/ratings.js';
 // import './components/dropdown.js';
 // import './components/tabs.js';
 // import './components/modals.js';
